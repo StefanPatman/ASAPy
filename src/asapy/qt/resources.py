25b497ede1bab27c1c5d7cac29d23a334ca7fc07
StefanPatman <stefanpatman91@gmail.com>
2023-01-30 18:19:47 +0200

    Update resources.get

--- src/asapy/qt/resources.py
+++ src/asapy/qt/resources.py
@@ -18,52 +18,9 @@
 
 """Get resource paths for Python3.8+"""
 
+from itaxotools.common.resources import package_path
 
-import pathlib
-import sys
-import os
-
-
-def _package_path_pyinstaller(package):
-    if isinstance(package, str):
-        parts = package.split('.')
-    elif isinstance(package, type(sys)):
-        parts = package.__name__.split('.')
-    else:
-        return None
-    path = pathlib.Path(sys._MEIPASS)
-    for part in parts:
-        path /= part
-    return path
-
-
-def _package_path_file(package):
-    if isinstance(package, str):
-        file = sys.modules[package].__file__
-    elif isinstance(package, type(sys)):
-        file = package.__file__
-    else:
-        return None
-    path = pathlib.Path(os.path.dirname(file))
-    return path
-
-
-def _package_path_importlib(package):
-    return importlib.resources.files(package)
-
-
-try:
-    import importlib.resources
-    from importlib.resources import files
-    package_path = _package_path_importlib
-except (ModuleNotFoundError, ImportError):
-    if hasattr(sys, '_MEIPASS'):
-        package_path = _package_path_pyinstaller
-    else:
-        package_path = _package_path_file
-
-_resource_path = package_path(__package__) / 'resources'
-
+_resource_path = package_path(__package__)
 
 def get(path):
-    return str(_resource_path / path)
+    return str(_resource_path / 'resources' / path)
